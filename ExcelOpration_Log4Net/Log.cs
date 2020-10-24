cef3181f6621aca484b59466935d398c64c6631c
jiaxing.zhang <zjx.la@outlook.com>
2020-10-24 15:00:33 +0800

    log4net

---
+++ ExcelOpration_Log4Net/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace ExcelOpration_Log4Net
+{
+    public class Log
+    {
+        public Log()
+        {
+            //配置文件
+            var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
+            //加载配置
+            XmlConfigurator.ConfigureAndWatch(logCfg);
+        }
+
+        public void Debug(string msg)
+        {
+            var debug = LogManager.GetLogger("debug");
+            debug.Debug(msg);
+        }
+
+        public void Info(string msg)
+        {
+            var info = LogManager.GetLogger("info");
+            info.Info(msg);
+        }
+
+        public void Error(string msg)
+        {
+            var error = LogManager.GetLogger("error");
+            error.Error(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            var warn = LogManager.GetLogger("warn");
+            warn.Warn(msg);
+        }
+    }
+}
